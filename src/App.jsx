5c3eef6246bbfc413a5c9e1b43df0227f05dd97a
aditrimehta <aditrixmehta@gmail.com>
2025-01-10 13:04:01 +0530

    initial changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <header>
+        <h1>doctor system</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
